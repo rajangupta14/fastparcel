427242f68716994d0ac7de89445a7f5fc24cce5f
rajangupta14 <rajanguptag14@gmail.com>
2021-06-29 07:13:09 -0700

    Initialize project

---
+++ core/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def HomePageView(request):
+    return render(request, 'HomePageView.html')
+# def CustomerPageView(request):
+#     return render(request, 'CustomerPageView.html')
+# def CourierPageView(request):
+#     return render(request, 'CourierPageView.html')
